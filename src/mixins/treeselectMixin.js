0e72df8b00e9bd7e74f65c04b78952aa2e6f6558
Jesse Smit <jesse@jessesmit.com>
2024-12-14 12:03:19 +0100

    attempt to fix e is null error

--- src/mixins/treeselectMixin.js
+++ src/mixins/treeselectMixin.js
@@ -1385,6 +1385,11 @@ export default {
         this.forest.nodeMap[prev].isHighlighted = false
       }
 
+      if (!node) {
+        this.menu.current = null
+        return
+      }
+
       this.menu.current = node.id
       node.isHighlighted = true
 
